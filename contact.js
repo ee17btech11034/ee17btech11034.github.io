531dc5ca21decd82c39f128576417762ff5fcad2
Asur_Raj <47558658+ee17btech11034@users.noreply.github.com>
2022-12-17 18:29:16 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,6 @@
+let Contactinfo = ()=>{
+    setTimeout(() => {
+        document.body = "OOps as it is in build stage so no post method is added till now....."
+    }, 2000);
+    location.href = '/';
+}
